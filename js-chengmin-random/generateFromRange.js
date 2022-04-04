de0504e6eb83f3bc58d8fbc7236544eb0b934cbb
Cmtheit <cmtheit@outlook.com>
2022-04-04 17:40:03 +0800

    Changes

---
+++ js-chengmin-random/generateFromRange.js
@@ -0,0 +1,42 @@
+/* 本模块从一个 range 参数的 0 和 1 下标生成一个包含介于它们之间的所有对象的数组
+   可定制函数API：generateFromRange 行数: 8
+   传入转换函数 transformMethod，下限函数 lowerBound 和上限函数 upperBound， 均为可选。
+   返回一个函数，接受若干个二元数组，对每个二元数组的第一个成员调用 lowerBound函数， 第二个成员调用 upperBound函数，分别得到下限和上限（均为整数）
+   而转换函数用于将每一个 下限和上限之间的值转换为新值，加入到返回的res数组中。
+   调用示例如下
+*/
+function generateFromRange(transformMethod, lowerBound, upperBound){
+    return function (...ranges) {
+        let res = [];
+        transformMethod = transformMethod || ((i) => i);
+        lowerBound = lowerBound || ((r) => r[0]);
+        upperBound = upperBound || ((r) => r[1]);
+        for (const range of ranges) {
+            for (let i = lowerBound(range); i <= upperBound(range); ++i) {
+                res.push(transformMethod(i));
+            }
+        }
+        return res;
+    }
+}
+
+
+// 生成的函数的调用见 constans.js 10、12、14、16行，此函数的作用一目了然
+// 此函数用于从所给出的二元字符或数字数组中得到范围内的连续字符数组。如 ['a', 'z'] 传入将返回：
+// [
+//   'a', 'b', 'c', 'd', 'e', 'f',
+//   'g', 'h', 'i', 'j', 'k', 'l',
+//   'm', 'n', 'o', 'p', 'q', 'r',
+//   's', 't', 'u', 'v', 'w', 'x',
+//   'y', 'z'
+// ]
+const generateCharsFromRange = generateFromRange(
+    String.fromCharCode,
+    (range) => range[0].charCodeAt ? range[0].charCodeAt(0) : range[0],
+    (range) => range[1].charCodeAt ? range[1].charCodeAt(0) : range[1]
+)
+
+// 将从整数上下限产生连续的整数数组（均为闭区间）
+const generateNumbersFromRange = generateFromRange();
+
+export {generateNumbersFromRange, generateCharsFromRange, generateFromRange }
---
+++ js-chengmin-random/randomChoice.js
@@ -0,0 +1,32 @@
+import {randomInt} from "./randomNumber.js";
+
+// 传入一系列可迭代对象（有length或size属性表示其大小）， 返回的函数每次调用随机从当初传入的所有可迭代对象抽取一个元素
+function randomChoice(...collector){
+    let totalLen = 0;
+    for (let i of collector){
+        totalLen += i.length || i.size;
+    }
+    let N = randomInt(1, totalLen);
+    for(const s of collector){
+        if (N <= (s.length || s.size)){
+            var theS = s;
+            break;
+        }else {
+            N -= (s.length || s.size);
+        }
+    }
+    for (var res of theS){
+        if(!--N){
+            break;
+        }
+    }
+    return res;
+}
+function test(){
+    let randomfromSet = randomChoice(new Set([1,3,4,5]), new Set([..."hello world!"]));
+    let randomFromArrayAndString = randomChoice([1,2,4,5,6], "hello world!");
+    console.log(randomfromSet, randomFromArrayAndString);
+}
+
+
+export {randomChoice}
